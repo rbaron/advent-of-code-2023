9509449a18e019a27c122917cf82bcde37242412
Raphael Baron <rbaron@rbaron.net>
2023-12-01 00:07:40 -0800

    Lil cleanup on day 01

--- day01/main.py
+++ day01/main.py
@@ -7,17 +7,11 @@ first_last = lambda l: int("".join((l[0], l[-1])))
 print(sum(first_last([c for c in l if "0" <= c <= "9"]) for l in lines))
 
 # Pt 2.
-VALS = {
-    "zero": 0,
-    "one": 1,
-    "two": 2,
-    "three": 3,
-    "four": 4,
-    "five": 5,
-    "six": 6,
-    "seven": 7,
-    "eight": 8,
-    "nine": 9,
+SUBS = {
+    s: str(i)
+    for i, s in enumerate(
+        ("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+    )
 }
 
 
@@ -26,9 +20,9 @@ def subs(line):
     for i, c in enumerate(line):
         if "0" <= c <= "9":
             out.append(c)
-        for k, v in VALS.items():
+        for k, v in SUBS.items():
             if k == line[i : i + len(k)]:
-                out.append(str(v))
+                out.append(v)
     return out
 
 
