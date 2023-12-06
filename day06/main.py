0de3cb7cd5e191bde10898f4288dc631316c826e
Raphael Baron <rbaron@rbaron.net>
2023-12-06 00:59:17 -0800

    Day 06

---
+++ day06/main.py
@@ -0,0 +1,36 @@
+import fileinput
+from math import sqrt, ceil
+
+# Test.
+records = [(7, 9), (15, 40), (30, 200)]
+records = [(71530, 940200)]
+
+# Real input for pt1 and pt2.
+# records = [(48, 255), (87, 1288), (69, 1117), (81, 1623)]
+records = [(48876981, 255128811171623)]
+
+# Pt 1.
+# p = 1
+# for t, d in records:
+#     s = 0
+#     for t_p in range(t):
+#         # Second degree equation.
+#         total_dist = (t - t_p) * t_p
+#         # print(f"{t=} {d=} {t_p=}: {total_dist=}")
+#         s += total_dist > d
+#     p *= s
+
+# print(p)
+
+
+# Pt 2.
+[(t0, d0)] = records
+a = -1
+b = t0
+c = -d0 + 1
+
+delta = b**2 - 4 * a * c
+x1 = (-b + sqrt(delta)) / (2 * a)
+x2 = (-b - sqrt(delta)) / (2 * a)
+
+print(x1, x2, ceil(x2) - ceil(x1))
